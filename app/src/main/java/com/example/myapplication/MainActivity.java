2c1af9e24acf3fd95a2caa3bd9b622eac2eb2d2d
Bhav <bhavikmakwana09@gmail.com>
2020-02-11 13:36:16 +0000

    First Commit Functional chat application on android using JPAKEPlus to establish a symmetric key. TODO: Make easier to use

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,114 @@
+package com.example.myapplication;
+
+import android.os.Bundle;
+import android.os.Handler;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+
+/**
+ * Created by Girish Bhalerao on 5/4/2017.
+ */
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private TextView mTextViewReplyFromServer;
+    private EditText mEditTextSendMessage;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        Log.d("button", "print test 1");
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button buttonSend = (Button) findViewById(R.id.btn_send);
+        Button buttonConnect = (Button) findViewById(R.id.btn_connect);
+        mEditTextSendMessage = (EditText) findViewById(R.id.edt_send_message);
+        mTextViewReplyFromServer = (TextView) findViewById(R.id.tv_reply_from_server);
+
+        buttonSend.setOnClickListener(this);
+        buttonConnect.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        Log.d("button", "print test 2");
+        switch (v.getId()) {
+
+            case R.id.btn_send:
+                Log.d("button", "send message");
+                Log.d("button", mEditTextSendMessage.getText().toString());
+                sendMessage(mEditTextSendMessage.getText().toString());
+                break;
+            case R.id.btn_connect:
+                Log.d("button", "connect button pressed");
+                Button btn = (Button) findViewById(R.id.btn_connect);
+                btn.setEnabled(false);
+                new JPAKEPlus((Button) findViewById(R.id.btn_connect)).execute((Button) findViewById(R.id.btn_connect));
+                break;
+        }
+    }
+
+    private void connect() {
+
+    }
+
+    private void sendMessage(final String msg) {
+
+        final Handler handler = new Handler();
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+
+                try {
+                    //Replace below IP with the IP of that device in which server socket open.
+                    //If you change port then change the port number in the server side code also.
+                    Socket s = new Socket("192.168.0.25", 9001);
+
+                    OutputStream out = s.getOutputStream();
+
+                    PrintWriter output = new PrintWriter(out);
+                    Log.d("button", msg);
+                    output.println(msg);
+                    output.flush();
+                    BufferedReader input = new BufferedReader(new InputStreamReader(s.getInputStream()));
+                    final String st = input.readLine();
+
+                    handler.post(new Runnable() {
+                        @Override
+                        public void run() {
+
+                            String s = mTextViewReplyFromServer.getText().toString();
+                            if (st.trim().length() != 0)
+                                mTextViewReplyFromServer.setText(s + "\nFrom Server : " + st);
+                        }
+                    });
+
+                    output.close();
+                    out.close();
+                    s.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        thread.start();
+    }
+
+
+
+
+}
---
+++ app/src/main/java/com/example/myapplication/RoundZero.java
@@ -0,0 +1,17 @@
+package com.example.myapplication;
+
+import java.util.ArrayList;
+
+public class RoundZero {
+    private ArrayList<Long> clientIDs = new ArrayList<>();
+
+    RoundZero() {};
+
+    public ArrayList<Long> getClientIDs() {
+        return clientIDs;
+    }
+
+    public void setClientIDs(ArrayList<Long> clientIDs) {
+        this.clientIDs = clientIDs;
+    }
+}
